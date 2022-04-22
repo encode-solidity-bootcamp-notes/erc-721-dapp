53ac5e67b05bf22456586c9df34c258c09f69247
the-vampiire <25523682+the-vampiire@users.noreply.github.com>
2022-04-22 13:26:59 -0500

    contract: VampSquad ERC-721

---
+++ solidity/contracts/VampSquad.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract VampSquad is ERC721("VampSquad", "VAMP") {}
